4f3f86e1d9d8f51f4c1141a5ff30f98c301f2420
Shawn Lu <reborntj@gmail.com>
2019-08-28 11:25:16 -0700

    Add LoggedOut RIB

---
+++ ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoggedOutViewController.swift
+//  TicTacToe
+//
+//  Created by Shawn Lu on 8/28/19.
+//  Copyright © 2019 Uber. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol LoggedOutPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+
+    weak var listener: LoggedOutPresentableListener?
+}
