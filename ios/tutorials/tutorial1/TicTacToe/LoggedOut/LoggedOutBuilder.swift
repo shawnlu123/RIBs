c23172c98166a400b22682195a6424082991b2af
Shawn Lu <reborntj@gmail.com>
2019-08-28 11:25:25 -0700

    Add LoggedOut RIB

--- ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutBuilder.swift
+++ ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutBuilder.swift
@@ -9,32 +9,32 @@
 import RIBs
 
 protocol LoggedOutDependency: Dependency {
-    // TODO: Declare the set of dependencies required by this RIB, but cannot be
-    // created by this RIB.
+  // TODO: Declare the set of dependencies required by this RIB, but cannot be
+  // created by this RIB.
 }
 
 final class LoggedOutComponent: Component<LoggedOutDependency> {
 
-    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+  // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
 }
 
 // MARK: - Builder
 
 protocol LoggedOutBuildable: Buildable {
-    func build(withListener listener: LoggedOutListener) -> LoggedOutRouting
+  func build(withListener listener: LoggedOutListener) -> LoggedOutRouting
 }
 
 final class LoggedOutBuilder: Builder<LoggedOutDependency>, LoggedOutBuildable {
 
-    override init(dependency: LoggedOutDependency) {
-        super.init(dependency: dependency)
-    }
+  override init(dependency: LoggedOutDependency) {
+    super.init(dependency: dependency)
+  }
 
-    func build(withListener listener: LoggedOutListener) -> LoggedOutRouting {
-        let component = LoggedOutComponent(dependency: dependency)
-        let viewController = LoggedOutViewController()
-        let interactor = LoggedOutInteractor(presenter: viewController)
-        interactor.listener = listener
-        return LoggedOutRouter(interactor: interactor, viewController: viewController)
-    }
+  func build(withListener listener: LoggedOutListener) -> LoggedOutRouting {
+    let component = LoggedOutComponent(dependency: dependency)
+    let viewController = LoggedOutViewController()
+    let interactor = LoggedOutInteractor(presenter: viewController)
+    interactor.listener = listener
+    return LoggedOutRouter(interactor: interactor, viewController: viewController)
+  }
 }
--- ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutViewController.swift
+++ ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutViewController.swift
@@ -11,12 +11,12 @@ import RxSwift
 import UIKit
 
 protocol LoggedOutPresentableListener: class {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+  // TODO: Declare properties and methods that the view controller can invoke to perform
+  // business logic, such as signIn(). This protocol is implemented by the corresponding
+  // interactor class.
 }
 
 final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
-
-    weak var listener: LoggedOutPresentableListener?
+  
+  weak var listener: LoggedOutPresentableListener?
 }
