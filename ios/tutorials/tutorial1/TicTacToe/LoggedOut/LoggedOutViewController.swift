297dcad1f023de63a2a7dd663d7c3ab476064cde
Shawn Lu <reborntj@gmail.com>
2019-08-28 11:50:25 -0700

    Finish tutorial 1

--- ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutViewController.swift
+++ ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutViewController.swift
@@ -75,6 +75,6 @@ final class LoggedOutViewController:
   }
 
   @objc private func didTapLoginButton() {
-
+    listener?.login(withPlayer1Name: player1Field?.text, player2Name: player2Field?.text)
   }
 }
