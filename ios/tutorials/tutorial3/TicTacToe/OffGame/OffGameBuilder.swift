9ac9c2f58776164628f34355cfc9edfe44b05050
Shawn Lu <reborntj@gmail.com>
2019-08-31 14:27:08 -0700

    Pass player names to OffGame RIB

--- ios/tutorials/tutorial3/TicTacToe/OffGame/OffGameBuilder.swift
+++ ios/tutorials/tutorial3/TicTacToe/OffGame/OffGameBuilder.swift
@@ -17,32 +17,38 @@
 import RIBs
 
 protocol OffGameDependency: Dependency {
-    // TODO: Declare the set of dependencies required by this RIB, but cannot be
-    // created by this RIB.
+  var player1Name: String { get }
+  var player2Name: String { get }
 }
 
 final class OffGameComponent: Component<OffGameDependency> {
-
-    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+  fileprivate var player1Name: String {
+    return dependency.player1Name
+  }
+  
+  fileprivate var player2Name: String {
+    return dependency.player2Name
+  }
 }
 
 // MARK: - Builder
 
 protocol OffGameBuildable: Buildable {
-    func build(withListener listener: OffGameListener) -> OffGameRouting
+  func build(withListener listener: OffGameListener) -> OffGameRouting
 }
 
 final class OffGameBuilder: Builder<OffGameDependency>, OffGameBuildable {
-
-    override init(dependency: OffGameDependency) {
-        super.init(dependency: dependency)
-    }
-
-    func build(withListener listener: OffGameListener) -> OffGameRouting {
-        _ = OffGameComponent(dependency: dependency)
-        let viewController = OffGameViewController()
-        let interactor = OffGameInteractor(presenter: viewController)
-        interactor.listener = listener
-        return OffGameRouter(interactor: interactor, viewController: viewController)
-    }
+  
+  override init(dependency: OffGameDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  func build(withListener listener: OffGameListener) -> OffGameRouting {
+    let component = OffGameComponent(dependency: dependency)
+    let viewController = OffGameViewController(player1Name: component.player1Name,
+                                               player2Name: component.player2Name)
+    let interactor = OffGameInteractor(presenter: viewController)
+    interactor.listener = listener
+    return OffGameRouter(interactor: interactor, viewController: viewController)
+  }
 }
--- ios/tutorials/tutorial3/TicTacToe/OffGame/OffGameViewController.swift
+++ ios/tutorials/tutorial3/TicTacToe/OffGame/OffGameViewController.swift
@@ -21,59 +21,108 @@ import SnapKit
 import UIKit
 
 protocol OffGamePresentableListener: class {
-    func startGame()
+  func startGame()
 }
 
-final class OffGameViewController: UIViewController, OffGamePresentable, OffGameViewControllable {
-
-    weak var listener: OffGamePresentableListener?
-
-    init() {
-        super.init(nibName: nil, bundle: nil)
+final class OffGameViewController:
+  UIViewController,
+  OffGamePresentable,
+  OffGameViewControllable
+{
+  weak var listener: OffGamePresentableListener?
+  
+  private let player1Name: String
+  private let player2Name: String
+  
+  init(player1Name: String, player2Name: String) {
+    self.player1Name = player1Name
+    self.player2Name = player2Name
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("Method is not supported")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = UIColor.yellow
+    buildStartButton()
+    buildPlayerLabels()
+  }
+  
+  // MARK: - Private
+  
+  private func buildPlayerLabels() {
+    let labelBuilder: (UIColor, String) -> UILabel = { (color: UIColor, text: String) in
+      let label = UILabel()
+      label.font = UIFont.boldSystemFont(ofSize: 35)
+      label.backgroundColor = UIColor.clear
+      label.textColor = color
+      label.textAlignment = .center
+      label.text = text
+      return label
     }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("Method is not supported")
+    
+    let player1Label = labelBuilder(UIColor.blue, player1Name)
+    view.addSubview(player1Label)
+    player1Label.snp.makeConstraints { (maker: ConstraintMaker) in
+      maker.top.equalTo(self.view).offset(70)
+      maker.leading.trailing.equalTo(self.view).inset(20)
+      maker.height.equalTo(40)
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = UIColor.yellow
-        buildStartButton()
+    
+    let vsLabel = UILabel()
+    vsLabel.font = UIFont.systemFont(ofSize: 25)
+    vsLabel.backgroundColor = UIColor.clear
+    vsLabel.textColor = UIColor.darkGray
+    vsLabel.textAlignment = .center
+    vsLabel.text = "vs"
+    view.addSubview(vsLabel)
+    vsLabel.snp.makeConstraints { (maker: ConstraintMaker) in
+      maker.top.equalTo(player1Label.snp.bottom).offset(10)
+      maker.leading.trailing.equalTo(player1Label)
+      maker.height.equalTo(20)
     }
-
-    // MARK: - Private
-
-    private func buildStartButton() {
-        let startButton = UIButton()
-        view.addSubview(startButton)
-        startButton.snp.makeConstraints { (maker: ConstraintMaker) in
-            maker.center.equalTo(self.view.snp.center)
-            maker.leading.trailing.equalTo(self.view).inset(40)
-            maker.height.equalTo(100)
-        }
-        startButton.setTitle("Start Game", for: .normal)
-        startButton.setTitleColor(UIColor.white, for: .normal)
-        startButton.backgroundColor = UIColor.black
-        startButton.rx.tap
-            .subscribe(onNext: { [weak self] in
-                self?.listener?.startGame()
-            })
-            .disposed(by: disposeBag)
+    
+    let player2Label = labelBuilder(UIColor.red, player2Name)
+    view.addSubview(player2Label)
+    player2Label.snp.makeConstraints { (maker: ConstraintMaker) in
+      maker.top.equalTo(vsLabel.snp.bottom).offset(10)
+      maker.height.leading.trailing.equalTo(player1Label)
     }
-
-    private let disposeBag = DisposeBag()
+  }
+  
+  private func buildStartButton() {
+    let startButton = UIButton()
+    view.addSubview(startButton)
+    startButton.snp.makeConstraints { (maker: ConstraintMaker) in
+      maker.center.equalTo(self.view.snp.center)
+      maker.leading.trailing.equalTo(self.view).inset(40)
+      maker.height.equalTo(100)
+    }
+    startButton.setTitle("Start Game", for: .normal)
+    startButton.setTitleColor(UIColor.white, for: .normal)
+    startButton.backgroundColor = UIColor.black
+    startButton.rx.tap
+      .subscribe(onNext: { [weak self] in
+        self?.listener?.startGame()
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  private let disposeBag = DisposeBag()
 }
 
 extension PlayerType {
-
-    var color: UIColor {
-        switch self {
-        case .player1:
-            return UIColor.red
-        case .player2:
-            return UIColor.blue
-        }
+  
+  var color: UIColor {
+    switch self {
+    case .player1:
+      return UIColor.red
+    case .player2:
+      return UIColor.blue
     }
+  }
 }
