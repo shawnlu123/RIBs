d5a987b17fc9e45baaea990de70b607e8dfc5630
Shawn Lu <reborntj@gmail.com>
2019-08-31 15:49:49 -0700

    Add URL handler

--- ios/tutorials/tutorial4/TicTacToe/Root/RootInteractor.swift
+++ ios/tutorials/tutorial4/TicTacToe/Root/RootInteractor.swift
@@ -18,44 +18,58 @@ import RIBs
 import RxSwift
 
 protocol RootRouting: ViewableRouting {
-    func routeToLoggedIn(withPlayer1Name player1Name: String, player2Name: String)
+  func routeToLoggedIn(withPlayer1Name player1Name: String, player2Name: String)
 }
 
 protocol RootPresentable: Presentable {
-    var listener: RootPresentableListener? { get set }
-    // TODO: Declare methods the interactor can invoke the presenter to present data.
+  var listener: RootPresentableListener? { get set }
+  // TODO: Declare methods the interactor can invoke the presenter to present data.
 }
 
 protocol RootListener: class {
-    // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+  // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
 }
 
-final class RootInteractor: PresentableInteractor<RootPresentable>, RootInteractable, RootPresentableListener {
-
-    weak var router: RootRouting?
-
-    weak var listener: RootListener?
-
-    // TODO: Add additional dependencies to constructor. Do not perform any logic
-    // in constructor.
-    override init(presenter: RootPresentable) {
-        super.init(presenter: presenter)
-        presenter.listener = self
-    }
-
-    override func didBecomeActive() {
-        super.didBecomeActive()
-        // TODO: Implement business logic here.
-    }
-
-    override func willResignActive() {
-        super.willResignActive()
-        // TODO: Pause any business logic.
-    }
-
-    // MARK: - LoggedOutListener
-
-    func didLogin(withPlayer1Name player1Name: String, player2Name: String) {
-        router?.routeToLoggedIn(withPlayer1Name: player1Name, player2Name: player2Name)
-    }
+final class RootInteractor:
+  PresentableInteractor<RootPresentable>,
+  RootInteractable,
+  RootPresentableListener,
+  RootActionableItem,
+  UrlHandler
+{
+  weak var router: RootRouting?
+  
+  weak var listener: RootListener?
+  
+  // TODO: Add additional dependencies to constructor. Do not perform any logic
+  // in constructor.
+  override init(presenter: RootPresentable) {
+    super.init(presenter: presenter)
+    presenter.listener = self
+  }
+  
+  override func didBecomeActive() {
+    super.didBecomeActive()
+    // TODO: Implement business logic here.
+  }
+  
+  override func willResignActive() {
+    super.willResignActive()
+    // TODO: Pause any business logic.
+  }
+  
+  // MARK: - LoggedOutListener
+  
+  func didLogin(withPlayer1Name player1Name: String, player2Name: String) {
+    router?.routeToLoggedIn(withPlayer1Name: player1Name, player2Name: player2Name)
+  }
+  
+  // MARK: - UrlHandler
+  
+  func handle(_ url: URL) {
+    let launchGameWorkflow = LaunchGameWorkflow(url: url)
+    launchGameWorkflow
+      .subscribe(self)
+      .disposeOnDeactivate(interactor: self)
+  }
 }
